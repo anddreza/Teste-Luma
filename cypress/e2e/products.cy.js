8a3558e7445f68caf2c177b8293e4d9419df4629
Andreza <dedezasilva@live.com>
2023-10-03 17:45:27 -0300

    feat: adição de cenário e métodos de consulta

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -8,25 +8,11 @@ describe('adicionar produto ao carrinho', () => {
 		cy.wait(5000)
 
 		ProductPage.fillWomen()
-
-		//Escolher produto Hot Sellers -> Radiant Tee
-		cy.contains('div[class="product-item-info"]', 'Radiant Tee')
-			.should('be.visible')
-			.click()
-		//Escolher produto com tamanho
-		cy.get('div[id="option-label-size-157-item-171"]')
-			.should('be.visible')
-			.click()
-		//Escolher produto com cor
-		cy.get('div[id="option-label-color-93-item-50"]')
-			.should('be.visible')
-			.click()
-
+		ProductPage.addElementWomen()
 		ProductPage.submit()
-
 	})
 
-	it.only('adicionar produto da tela de Tops ao carrinho', () => {
+	it('adicionar produto da tela de Tops ao carrinho', () => {
 		const user = login.usuario
 		cy.adminLogin()
 
@@ -39,6 +25,5 @@ describe('adicionar produto ao carrinho', () => {
 		cy.get('div[id="option-label-color-93-item-57"]')
 			.should('be.visible')
 			.click()
-		
 	})
 }) 
--- cypress/support/pages/ProductPage.js
+++ cypress/support/pages/ProductPage.js
@@ -1,10 +1,25 @@
 class ProductPage{
 	go(){
-		//tela interativa, devemos seguir e verificar onde devemos estar, essa função foi criada somente para registro
+		//tela interativa, devemos seguir e verificar urls, essa função foi criada somente para registro
 	}
 
 	fillWomen(){
 		cy.contains('a[class="level-top ui-corner-all"]', 'Women')
+			.should('be.visible')
+			.click()
+	}
+
+	addElementWomen(){
+		//Escolher produto Hot Sellers -> Radiant Tee
+		cy.contains('div[class="product-item-info"]', 'Radiant Tee')
+			.should('be.visible')
+			.click()
+		//Escolher produto com tamanho
+		cy.get('div[id="option-label-size-157-item-171"]')
+			.should('be.visible')
+			.click()
+		//Escolher produto com cor
+		cy.get('div[id="option-label-color-93-item-50"]')
 			.should('be.visible')
 			.click()
 	}
