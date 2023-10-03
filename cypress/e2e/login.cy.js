622d0ad4786726023cfbd05eb3f3724e592c794d
Andreza <dedezasilva@live.com>
2023-10-03 00:23:58 -0300

    feat: agrupamento todos em um lugar doLogin

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,86 +1,62 @@
 import data from '../fixtures/login.json'
 import LoginPage from '../support/pages/LoginPage'
-describe('login', ()=> {
+describe('login', () => {
 	//fixado um perfil 
 	it('deve logar com qualquer perfil', () => {
-		
+
 		const user = data.usuario
-		LoginPage.go()
-		LoginPage.fill(user)
-		LoginPage.submit()
+		LoginPage.doLogin(user)
 		cy.wait(10000)
 
 		//Welcome, Veronica Costello!
-		cy.contains('span.logged-in', 'Welcome, '+user.name+'!')
+		cy.contains('span.logged-in', 'Welcome, ' + user.name + '!')
 			.should('be.visible')
-			
-
 	})
 
 	it('não deve logar com senha incorreta', () => {
-
 		const user = data.inv_pass
 
-		LoginPage.go()
-		LoginPage.fill(user)
-		LoginPage.submit()
+		LoginPage.doLogin(user)
 
 		cy.contains('div.messages', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
-		    .should('be.visible')
+			.should('be.visible')
 	})
 
 	it('não deve logar com email não cadastrado', () => {
-
 		const user = data.email_not_found
 
-		LoginPage.go()
-		LoginPage.fill(user)
-		LoginPage.submit()
+		LoginPage.doLogin(user)
 
 		cy.contains('div.messages', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
-		    .should('be.visible')
+			.should('be.visible')
 	})
 
 	it('não deve logar com email incorreto', () => {
-
 		const user = data.inv_email
 
-		LoginPage.go()
-		LoginPage.fill(user)
-		LoginPage.submit()
+		LoginPage.doLogin(user)
 
 		cy.wait(5000)
-		 cy.get('div[id="email-error"]')
+		cy.get('div[id="email-error"]')
 			.should('be.visible')
-			.should('have.text', 'Please enter a valid email address (Ex: johndoe@domain.com).') 
-		    
+			.should('have.text', 'Please enter a valid email address (Ex: johndoe@domain.com).')
 	})
 
 	it('não deve logar com email em branco', () => {
-
 		const user = data.empty_email
 
-		LoginPage.go()
-		LoginPage.fill(user)
-		LoginPage.submit()
+		LoginPage.doLogin(user)
 
 		cy.contains('div.messages', 'A login and a password are required.')
-		    .should('be.visible')
+			.should('be.visible')
 	})
 
 	it('não deve logar com senha em branco', () => {
-
 		const user = data.empty_pass
 
-		LoginPage.go()
-		LoginPage.fill(user)
-		LoginPage.submit()
+		LoginPage.doLogin(user)
 
 		cy.contains('div.messages', 'A login and a password are required.')
-		    .should('be.visible')
+			.should('be.visible')
 	})
-
-
-
-	
 })
